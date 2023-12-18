8fd821ec0ddbbfa5f246ac20cdde0aab5748ac1f
kate59040 <143742588+kate59040@users.noreply.github.com>
2023-12-18 19:58:20 +0700

    Delete Django_p

--- Django_p/Django_project/app1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('blogpost/', views.blogpost_list, name='blogpost'),
-
-]
--- Django_p/Django_project/app1/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from .models import Blog
-
-
-def blogpost_list(request):
-    blogposts = {'blogposts': Blog.objects.order_by('-date')}
-    return render(request, 'blogpost.html', blogposts)
